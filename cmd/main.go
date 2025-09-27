cc8a9b7de424dca285694bc864fc787cac278402
Sam <samalizadeh899@gmail.com>
2025-09-27 15:48:59 +0330

    go mod tidy

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,38 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"simple_blockchain/pkg/HttpServer"
 	"simple_blockchain/pkg/LevelDB"
 	"simple_blockchain/pkg/blockchain"
+	"simple_blockchain/pkg/database"
 	"simple_blockchain/pkg/handler"
 )
 
-func main() {
-	var (
-		httpPort = flag.String("http-port", "8000", "Http server port")
-	)
-	flag.Parse()
+const Port = "8000"
 
-	dbInstance, err := LevelDB.New("balances")
+func main() {
+	sqliteDBInstance, err := database.New("sqlite3", "./blockchain_db.sqlite2")
 	if err != nil {
 		panic(err)
 	}
-	defer dbInstance.Close()
+	defer sqliteDBInstance.Close()
+
+	levelDBInstance, err := LevelDB.New("balances")
+	if err != nil {
+		panic(err)
+	}
+	defer levelDBInstance.Close()
 
 	var newBc *blockchain.Blockchain
 	var newMempool *blockchain.Mempool
 
-	newBc = blockchain.NewBlockchain("genesis-address", dbInstance)
+	newBc = blockchain.NewBlockchain("genesis-address", levelDBInstance, sqliteDBInstance)
 	newMempool = blockchain.NewMempool()
 
 	newHandler := handler.New(newBc, newMempool)
 
-	httpServer := HttpServer.New(*httpPort, newHandler)
+	httpServer := HttpServer.New(Port, newHandler)
 
 	if err := httpServer.Run(); err != nil {
 		log.Fatal(err)
