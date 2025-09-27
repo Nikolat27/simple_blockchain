c22daa2963ae52cce7343181aa6ce251517a7550
Sam <samalizadeh899@gmail.com>
2025-09-27 16:44:08 +0330

    Added new util file

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"simple_blockchain/pkg/HttpServer"
 	"simple_blockchain/pkg/blockchain"
 	"simple_blockchain/pkg/database"
 	"simple_blockchain/pkg/handler"
-
-	"github.com/joho/godotenv"
+	"simple_blockchain/pkg/utils"
 )
 
 const Port = "8000"
 
 func main() {
-	if err := loadEnv(); err != nil {
+	if err := utils.LoadEnv(); err != nil {
 		panic(err)
 	}
 
@@ -42,21 +40,4 @@ func main() {
 	if err := httpServer.Run(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func loadEnv() error {
-	if _, err := os.Stat(".env"); os.IsNotExist(err) {
-		file, err := os.Create(".env")
-		if err != nil {
-			return fmt.Errorf("error creating .env file: %v", err)
-		}
-
-		defer file.Close()
-	}
-
-	if err := godotenv.Load(); err != nil {
-		return fmt.Errorf("failed to load the .env variables")
-	}
-
-	return nil
 }
