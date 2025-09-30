6f8d98d582fb4ce12193157c6cc70a1b9dd5fcce
Sam <samalizadeh899@gmail.com>
2025-09-30 20:56:03 +0330

    Added Peers table (for data persistency)

---
+++ migrations/3_create_peers_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+
+CREATE TABLE IF NOT EXISTS peers
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    tcp_address TEXT UNIQUE NOT NULL
+);
+
+CREATE INDEX idx_peers_tcp_address ON peers (tcp_address);
+
+-- +goose Down
+DROP TABLE IF EXISTS peers;
+DROP INDEX IF EXISTS idx_peers_tcp_address
