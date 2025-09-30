be77767c615929d19dcf5773962ca1af76d44f5c
Sam <samalizadeh899@gmail.com>
2025-09-30 19:54:06 +0330

    Msg parser of p2p msgs

---
+++ pkg/p2p/NodeMessageParsing.go
@@ -0,0 +1,38 @@
+package p2p
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+func (node *Node) parseMessage(senderMsg []byte) error {
+	var msg Message
+
+	if err := json.Unmarshal(senderMsg, &msg); err != nil {
+		return err
+	}
+
+	if msg.SenderAddress == "" {
+		return errors.New("msg senderAddress field is empty")
+	}
+
+	switch msg.Type {
+
+	case JoinNetworkMsg:
+		return node.handleNodeJoinNetwork(msg.SenderAddress)
+
+		// Requesting the blockchain`s data
+	case SendBlockchainDataMsg:
+		return node.handleGetBlockchainData(msg.SenderAddress)
+
+		// Sending the blockchain`s data to the applicant
+	case GetBlockchainDataMsg:
+		node.blockchainRespCh <- msg.Blocks
+
+	default:
+		fmt.Println("meow meow")
+	}
+
+	return nil
+}
