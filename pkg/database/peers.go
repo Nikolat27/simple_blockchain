68cd474cee1cc02bcc0a2220669d38453cc85569
Sam <samalizadeh899@gmail.com>
2025-09-30 20:56:19 +0330

    Added peers related methods

---
+++ pkg/database/peers.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+)
+
+func (db *Database) AddPeer(sqlTx *sql.Tx, tcpAddress string) error {
+	query := `
+		INSERT OR IGNORE INTO peers(tcp_address)
+		VALUES (?);
+	`
+
+	if _, err := sqlTx.Exec(query, tcpAddress); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *Database) GetPeers() (*sql.Rows, error) {
+	query := `
+		SELECT tcp_address FROM peers;
+	`
+
+	return db.db.Query(query, nil)
+}
+
+func (db *Database) PeerExist(tcpAddress string) (bool, error) {
+	query := `
+		SELECT 1 FROM peers WHERE tcp_address = ? LIMIT 1
+	`
+
+	var exists int
+	if err := db.db.QueryRow(query, tcpAddress).Scan(&exists); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+
+		return false, nil
+	}
+
+	return true, nil
+}
