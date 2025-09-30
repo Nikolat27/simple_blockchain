f162676425bfa4d8bd5936c81dad7ff8af0901db
Sam <samalizadeh899@gmail.com>
2025-09-30 19:50:00 +0330

    Added json struct tags

--- pkg/blockchain/transaction.go
+++ pkg/blockchain/transaction.go
@@ -13,10 +13,10 @@ type Transaction struct {
 	From       string `json:"from,omitempty"`
 	To         string `json:"to"`
 	Amount     uint64 `json:"amount"`
-	Timestamp  int64
+	Timestamp  int64  `json:"timestamp"`
 	PublicKey  string // sender's public key (hex string)
 	Signature  []byte // Ed25519 signature
-	Fee        uint64
+	Fee        uint64 `json:"fee"`
 	Status     string `json:"status"`
 	IsCoinbase bool   `json:"is_coinbase"`
 }
