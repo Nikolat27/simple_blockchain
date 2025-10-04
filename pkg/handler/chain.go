29e3fe2bc8cc03c288844e709c88442ac20cc1f8
Sam <samalizadeh899@gmail.com>
2025-10-04 13:13:57 +0330

    Added GetMempool func

--- pkg/handler/chain.go
+++ pkg/handler/chain.go
@@ -12,3 +12,12 @@ func (handler *Handler) GetBlockchain(w http.ResponseWriter, r *http.Request) {
 
 	utils.WriteJSON(w, http.StatusOK, resp)
 }
+
+func (handler *Handler) GetMempool(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]any{
+		"total":   len(handler.Node.Blockchain.Mempool.Transactions),
+		"mempool": handler.Node.Blockchain.Mempool,
+	}
+
+	utils.WriteJSON(w, http.StatusOK, resp)
+}
