dc031f8e679e208600fe06143652e7450e6b5057
Sam <samalizadeh899@gmail.com>
2025-10-05 19:33:20 +0330

    Formatted the sql query

--- migrations/2_create_blocks_and_txs_table.sql
+++ migrations/2_create_blocks_and_txs_table.sql
@@ -1,36 +1,31 @@
 -- +goose Up
-CREATE TABLE IF NOT EXISTS blocks
-(
-    id           INTEGER Primary Key AUTOINCREMENT,
-    prev_hash    TEXT UNIQUE NOT NULL,
-    hash         TEXT UNIQUE NOT NULL,
-    merkle_root  TEXT UNIQUE NOT NULL,
-    nonce        INTEGER DEFAULT (0),
-    timestamp    INTEGER DEFAULT (strftime('%s', 'now')),
+CREATE TABLE IF NOT EXISTS blocks (
+    id INTEGER Primary Key AUTOINCREMENT,
+    prev_hash TEXT UNIQUE NOT NULL,
+    hash TEXT UNIQUE NOT NULL,
+    merkle_root TEXT UNIQUE NOT NULL,
+    nonce INTEGER DEFAULT (0),
+    timestamp INTEGER DEFAULT (strftime('%s', 'now')),
     block_height INTEGER DEFAULT (0)
 );
-
-CREATE TABLE IF NOT EXISTS transactions
-(
-    id           INTEGER PRIMARY KEY AUTOINCREMENT,
-    block_id     INTEGER NULL,
-    sender       TEXT    NULL,
-    recipient    TEXT    NOT NULL,
-    amount       INTEGER          DEFAULT (0),
-    fee          INTEGER          DEFAULT (0),
-    timestamp    INTEGER          DEFAULT (strftime('%s', 'now')),
-    public_key   TEXT    NULL,
-    signature    TEXT    NULL,
-    status       TEXT    NOT NULL DEFAULT ('pending') CHECK (status IN ('pending', 'confirmed')),
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    block_id INTEGER NULL,
+    sender TEXT NULL,
+    recipient TEXT NOT NULL,
+    amount INTEGER DEFAULT (0),
+    fee INTEGER DEFAULT (0),
+    timestamp INTEGER DEFAULT (strftime('%s', 'now')),
+    public_key TEXT NULL,
+    signature TEXT NULL,
+    status TEXT NOT NULL DEFAULT ('pending') CHECK (status IN ('pending', 'confirmed')),
     is_coin_base INTEGER NOT NULL DEFAULT (0) CHECK (is_coin_base IN (0, 1)),
-    FOREIGN KEY (block_id) REFERENCES blocks (id) ON DELETE SET NULL
+    FOREIGN KEY (block_id) REFERENCES blocks (id) ON DELETE
+    SET NULL
 );
-
 -- indexes
 CREATE INDEX idx_transactions_block_id ON transactions (block_id);
-
 -- +goose Down
 DROP INDEX IF EXISTS idx_transactions_block_id;
-
 DROP TABLE IF EXISTS blocks;
 DROP TABLE IF EXISTS transactions;
--- migrations/3_create_peers_table.sql
+++ migrations/3_create_peers_table.sql
@@ -1,13 +1,9 @@
 -- +goose Up
-
-CREATE TABLE IF NOT EXISTS peers
-(
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS peers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     tcp_address TEXT UNIQUE NOT NULL
 );
-
 CREATE INDEX idx_peers_tcp_address ON peers (tcp_address);
-
 -- +goose Down
 DROP TABLE IF EXISTS peers;
 DROP INDEX IF EXISTS idx_peers_tcp_address
